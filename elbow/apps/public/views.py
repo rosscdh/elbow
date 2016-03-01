98d89c0dc3fcb6f38c822965eb07ade51a15e0f2
Ross Crawford-d'Heureuse <rosscdh@s.local>
2016-03-01 17:55:30 +0100

    perm homepage redirect to start page

--- elbow/apps/public/views.py
+++ elbow/apps/public/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
-
+from django.views.generic import RedirectView
 from elbow.apps.project.models import Project
 
 
-class HomePageView(TemplateView):
+#class HomePageView(TemplateView):
+class HomePageView(RedirectView):
+    url = '/de/p/start/'
     template_name = 'home/home.html'
 
     def get_context_data(self):
