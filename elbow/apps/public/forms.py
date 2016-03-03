e763a4f18cb4f4cdc8573e22b978c1edb95140a9
Ross Crawford-d'Heureuse <rosscdh@s.local>
2016-03-03 16:27:55 +0100

    added first name last name to register

--- elbow/apps/public/forms.py
+++ elbow/apps/public/forms.py
@@ -1,10 +1,18 @@
 # -*- coding: UTF-8 -*-
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Field, Submit, HTML
+from crispy_forms.bootstrap import PrependedAppendedText
+
 from allauth.account.forms import SignupForm as AllAuthSignupForm
 
 
 class SignUpForm(AllAuthSignupForm):
+    first_name = forms.CharField(label=_('First name'), required=True)
+    last_name = forms.CharField(label=_('Last name'), required=True)
+
     has_aggeed_t_and_c = forms.BooleanField(label='',
                                             help_text=_('I have read & agree to the Terms & Conditions and Data Protection agreement'),
                                             required=True,
@@ -13,3 +21,29 @@ class SignUpForm(AllAuthSignupForm):
                                                 help_text=_('I would like to receive occasional news and information'),
                                                 widget=forms.CheckboxInput)
 
+    @property
+    def helper(self):
+        helper = FormHelper(self)
+
+        helper.form_action = ''
+        helper.form_show_errors = True
+        helper.render_unmentioned_fields = True
+
+        helper.layout = Layout(
+                               Fieldset(_('Personal Details'),
+                                  Field('first_name', css_class='col-sm-6'),
+                                  Field('last_name', css_class='col-sm-6'),
+                               ),
+                               Fieldset(_('Login Information'),
+                                  Field('email'),
+                                  Field('password1'),
+                                  Field('password2'),
+                               ),
+                               Fieldset(_('Agreement & Newsletter'),
+                                  Field('has_aggeed_t_and_c'),
+                                  Field('send_news_and_info'),
+                               ),
+                               ButtonHolder(
+                                    Submit('submit', _('Register'), css_class='btn btn-success btn-lg'),
+                               ))
+        return helper
