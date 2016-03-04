ba11b6bcc0ded0d97cab851ad837a18522881118
Ross Crawford-d'Heureuse <rosscdh@s.local>
2016-03-04 08:16:57 +0100

    added extra document separation layer

--- elbow/apps/document/admin.py
+++ elbow/apps/document/admin.py
@@ -4,9 +4,39 @@ from .models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ('uuid', 'name', 'document_type', 'created_at', 'status')
+    list_display = ('name', 'uuid', 'document_type', 'created_at', 'status')
     list_filter = ('document_type', 'status',)
     search_fields = ('uuid', 'name', 'document_type', 'status')
 
 
+class ProjectDocument(Document):
+    class Meta:
+        proxy = True
+
+
+class ProjectDocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'uuid', 'created_at', 'status')
+    list_filter = ('document_type', 'status',)
+    search_fields = ('uuid', 'name', 'status')
+
+    def get_queryset(self, request):
+        return super(ProjectDocumentAdmin, self).get_queryset(request).filter(document_type='project')
+
+
+class OrderDocument(Document):
+    class Meta:
+        proxy = True
+
+
+class OrderDocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'uuid', 'created_at', 'status')
+    list_filter = ('status',)
+    search_fields = ('uuid', 'name', 'status')
+
+    def get_queryset(self, request):
+        return super(OrderDocumentAdmin, self).get_queryset(request).filter(document_type__in=['order', 'loan_agreement'])
+
+
 admin.site.register(Document, DocumentAdmin)
+admin.site.register(ProjectDocument, ProjectDocumentAdmin)
+admin.site.register(OrderDocument, OrderDocumentAdmin)
