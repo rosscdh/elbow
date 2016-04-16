a9ed56eba5ec70bd58ef381e8921b0c0eeec339c
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2016-04-16 16:22:49 +0200

    added base url

--- elbow/context_processors.py
+++ elbow/context_processors.py
@@ -5,4 +5,5 @@ from django.conf import settings
 def elbow_globals(request):
     return {
         'SECUPAY_DEMO': getattr(settings, 'SECUPAY_DEMO', False),
+        'BASE_URL': getattr(settings, 'BASE_URL', None),
     }
