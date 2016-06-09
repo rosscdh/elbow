a6945c01dae6366ceb55eebd21787679792a431b
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2016-06-09 17:13:22 +0200

    fixed url to pdfs

--- elbow/apps/public/tests/test_user_signup_email.py
+++ elbow/apps/public/tests/test_user_signup_email.py
@@ -43,5 +43,4 @@ class EmailsSentOnNewSignupTest(BaseTestCase):
         email = mail.outbox[1]  # Customer Email
         self.assertEqual(email.subject, u'Registrierung abschlie\xdfen auf TodayCapital')
         self.assertEqual(email.recipients(), [u'test+user@example.com'])
-        import pdb;pdb.set_trace()
 
