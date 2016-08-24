b2598f246e5ed04ba9403560f24c892b1f3ed745
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2016-08-24 12:34:06 +0200

    added SECUPAY_BANK_DATA to payment view

--- elbow/apps/document/views.py
+++ elbow/apps/document/views.py
@@ -3,6 +3,7 @@ from django.http import FileResponse
 from django.views.generic import DetailView
 
 from elbow.mixins import LoginRequiredMixin
+from elbow.apps.order.apps import SECUPAY_BANK_DATA
 
 from .models import Document
 
@@ -13,6 +14,13 @@ class DocumentDownload(LoginRequiredMixin, DetailView):
     slug_field = 'uuid'
     slug_url_kwarg = 'uuid'
     content_type = 'application/pdf'
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(DocumentDownload, self).get_context_data(**kwargs)
+        kwargs.update({
+            'SECUPAY_BANK_DATA': SECUPAY_BANK_DATA,
+        })
+        return kwargs
 
     def render_to_response(self, *args, **kwargs):
         return FileResponse(self.get_object().document, content_type=self.content_type)
