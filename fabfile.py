3a0980f42b9e95ea899820e7429060b3fa579878
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2017-02-20 20:46:42 +0100

    pdates

--- fabfile.py
+++ fabfile.py
@@ -625,7 +625,6 @@ def upload_db():
 
 @task
 def db_backup(db='default'):
-    os.environ['DJANGO_ENV'] = env.environment
     from django.conf import settings
     db_settings = getattr(settings, 'DATABASES')
 
